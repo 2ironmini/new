79fc9e44b0246500c9f2fce44f22f8de11334509
2ironmini <2ironmini@gmail.com>
2022-10-21 07:48:15 +0000

    up

--- dataload.js
+++ dataload.js
@@ -1,43 +1,20 @@
 $(document).ready(function () {
     $.getJSON('/project/carddata.json', function (data) {
         $("#cards").next().text(data.cards);
-            const CARD = data.cards;
-            const max00 = CARD.length;
-            for (let i = 0; i < max00; i++) {
-                const cloneEl = $("#cards").clone(true);
-                $(cloneEl).children("#name").attr(data.cards.name);
-                $(cloneEl).children("#manaCost").attr("manaCost");
-                
-                $(cloneEl).appendTo("#here"); //ul에 붙이기
-                // $(cloneEl).children("name").attr("name", CARD[i].name);
-                // $(cloneEl).children("classId").attr("classId", CARD[i].classId);
-                // $(cloneEl).children("rarity").attr("rarity", CARD[i].rarityId);
-                // $(cloneEl).children("artist").attr("artist", CARD[i].artistName);
-                // $(cloneEl).children("manaCost").attr("manaCost", CARD[i].manaCost);
-                // $(cloneEl).children("text").attr("text", CARD[i].text);
-                // $(cloneEl).children("flavorText").attr("flavorText", CARD[i].flavorText);
-                // $(cloneEl).children("image").attr("image", CARD[i].image);
-                // $(cloneEl).children("cardtype").attr("cardType", CARD[i].cardTypeId);
-                // $(cloneEl).children("minionType").attr("minionTypeId", CARD[i].minionTypeId);
-                // $(cloneEl).children("spellType").attr("spellType", CARD[i].spellSchoolId);
-                // $(cloneEl).children("health").attr("health", CARD[i].health);
-                // $(cloneEl).children("attack").attr("attack", CARD[i].attack);
-                
-            };
-        // const CARD = data.name;
-        // const max00 = data.name;
-        
-        // for (let i = 0; i < max00; i++){
-        //     const cloneE1 = $(".card").clone(true);
-        //     $(cloneEl).children("#2").text(CARD[i + 1].name);
-        //     $(cloneEl).insertAfter("#2");
-        // }
-        // {
-        //     const cloneE1 = $(".card").clone(true);
-        //     $(cloneE1).appendTo("img");
-            
-        // }
 
 
-    });
+	
+const lib = data.cards;
+const URL = lib.image;
+const max00 = lib.length;
+
+for (let i = 0; i < max00; i++)
+{
+const CARD = $("#cards").children().last().clone(true);
+$(CARD).children("#image > img").attr({alt: '0 ++', src: 'https://d15f34w2p8l1cc.cloudfront.net/hearthstone/fde1fb781e66ca5e482a37cd2b64fa058a8eee98177185a5d32bb2da614a86f8.png'});
+$(CARD).appendTO("#lib");
+}
+
+
+    });   
 });
--- hearthstone.js
+++ hearthstone.js
@@ -35,13 +35,6 @@ $(function() {
     $(".gnb .navmenu li .min:eq(0)").stop().trigger("mouseenter");     
 });
 
-$(document).ready(function(index, li){
-    $("#slide > .right > li > .guide > li").addClass(function(index){
-        return `intro${index+1}`;
-    });
-});
-
-
 
 $(function(){
     $(".gnb .navmenu .m").on("mouseenter focusin click", function(){
@@ -55,11 +48,18 @@ $(function(){
 });
 
 // -----------------------------------------------------all nav depth
+
+
+$("*").removeClass("on").stop().trigger("mouseleave, focusout");
+
+
+
+
 $(".gnb .navmenu .m:first a").click(function(){
-    $(".sec1").addClass("on").siblings().removeClass("on");
+    $("#sec1").addClass("on").siblings().removeClass("on");
 });    
 $(".gnb .navmenu .m:nth(1) a").click(function(){
-    $(".sec2").addClass("on").siblings().removeClass("on");
+    $("#sec2").addClass("on").siblings().removeClass("on");
 });    
 $(".gnb .navmenu .m:nth(2) a").click(function(){
     $("#sec3").addClass("on").siblings().removeClass("on");
@@ -96,6 +96,37 @@ $(".gnb .navmenu .m:first .snb li:first a").click(function(){
 $(".gnb .navmenu .m:first .snb li:last a").click(function(){
     $("#slide").addClass("on").siblings().removeClass("on");
 });    
+
+
+
+// -----------------------------------------------------
+
+// -----------------------------------------------------HSintro depth
+
+$(function() {
+    $("#whatHS .slidebt span:last").click(function() {
+        if (!$(".HSintro li").last().is(":visible")) {
+            $(".HSintro li:visible").hide().next("li").fadeIn(400);
+            $("#whatHS .slidebt span:first").removeClass("off");
+        }
+
+        if ($(".HSintro li").last().is(":visible")) {
+            $("#whatHS .slidebt span:last").addClass("off");
+        }
+        return false;
+    });
+    $("#whatHS .slidebt span:first").click(function() {
+        if (!$(".HSintro li").first().is(":visible")) {
+            $(".HSintro li:visible").hide().prev("li").fadeIn(400);
+            $("#whatHS .slidebt span:last").removeClass("off");
+        }
+
+        if ($(".HSintro li").first().is(":visible")) {
+            $("#whatHS .slidebt span:first").addClass("off");
+        }
+        return false;
+    });
+});
 
 
 
@@ -161,61 +192,133 @@ $(".sec2 .herose .Hr:last a").click(function(){
 
 // -----------------------------------------------------
 
+$(function() {
+    $("#slide .control li:first").click(function() {
+        $(".guide li:first").addClass("on").siblings().removeClass("on").fadeOut();
+        $(".textguide li:first").addClass("on").siblings().removeClass("on").fadeOut();
+    });
+    $("#slide .control li:nth(1)").click(function() {
+        $(".guide li:nth(1)").addClass("on").siblings().removeClass("on").fadeOut();
+        $(".textguide li:nth(1)").addClass("on").siblings().removeClass("on").fadeOut();
+    });
+    $("#slide .control li:nth(2)").click(function() {
+        $(".guide li:nth(2)").addClass("on").siblings().removeClass("on").fadeOut();
+        $(".textguide li:nth(2)").addClass("on").siblings().removeClass("on").fadeOut();
+    });
+    $("#slide .control li:nth(3)").click(function() {
+        $(".guide li:nth(3)").addClass("on").siblings().removeClass("on").fadeOut();
+        $(".textguide li:nth(3)").addClass("on").siblings().removeClass("on").fadeOut();
+    });
+    $("#slide .control li:nth(4)").click(function() {
+        $(".guide li:nth(4)").addClass("on").siblings().removeClass("on").fadeOut();
+        $(".textguide li:nth(4)").addClass("on").siblings().removeClass("on").fadeOut();
+    });
+    $("#slide .control li:nth(5)").click(function() {
+        $(".guide li:nth(5)").addClass("on").siblings().removeClass("on").fadeOut();
+        $(".textguide li:nth(5)").addClass("on").siblings().removeClass("on").fadeOut();
+    });
+    $("#slide .control li:nth(6)").click(function() {
+        $(".guide li:nth(6)").addClass("on").siblings().removeClass("on").fadeOut();
+        $(".textguide li:nth(6)").addClass("on").siblings().removeClass("on").fadeOut();
+    });
+    $("#slide .control li:nth(7)").click(function() {
+        $(".guide li:nth(7)").addClass("on").siblings().removeClass("on").fadeOut();
+        $(".textguide li:nth(7)").addClass("on").siblings().removeClass("on").fadeOut();
+    });
+    $("#slide .control li:nth(8)").click(function() {
+        $(".guide li:nth(8)").addClass("on").siblings().removeClass("on").fadeOut();
+        $(".textguide li:nth(8)").addClass("on").siblings().removeClass("on").fadeOut();
+    });
+        
+    
+});
+
+
+$(document).ready(function(index, li){
+    $(".slide > .control > li").addClass(function(index){
+        return `mana${index+1}`;
+    });
+});
+
+
+
+
+
+// $("#whatHS .slidebt span:last").click(function() {
+//     if (!$(".HSintro li").last().is(":visible")) {
+//         $(".HSintro li:visible").hide().next("li").fadeIn(400);
+//         $("#whatHS .slidebt span:first").removeClass("off");
+//     }
+
+//     if ($(".HSintro li").last().is(":visible")) {
+//         $("#whatHS .slidebt span:last").addClass("off");
+//     }
+//     return false;
+// });
+
+
+
+
+
+
+
 
 // -----------------------------------------------------#slide 컨트롤
 
 
-$("#slide > .control > li:first").click(function(){
-    $("#slide > .right > li > .guide > li:first").addClass("on").siblings().removeClass("on");
-    $("#slide > .right > li > .textguide li:first").addClass("on").siblings().removeClass("on");
+
+
+$("#slide > .control > li").click(function(){
+    $("#slide > .right > li > .guide").addClass("on").siblings();
+    $("#slide > .right > li > .textguide").addClass("on").siblings();
 });
     
 
-$("#slide > .control > li:nth(1)").click(function(){
-    $("#slide > .right > li > .guide > li:nth(1)").addClass("on").siblings().removeClass("on");
-    $("#slide > .right > li > .textguide li:nth(1)").addClass("on").siblings().removeClass("on");
-});
+// $("#slide > .control > li:nth(1)").click(function(){
+//     $("#slide > .right > li > .guide > li:nth(1)").addClass("on").siblings().removeClass("on");
+//     $("#slide > .right > li > .textguide li:nth(1)").addClass("on").siblings().removeClass("on");
+// });
 
-$("#slide > .control > li:nth(2)").click(function(){
-    $("#slide > .right > li > .guide > li:nth(2)").addClass("on").siblings().removeClass("on");
-    $("#slide > .right > li > .textguide > li:nth(2)").addClass("on").siblings().removeClass("on");
-});
+// $("#slide > .control > li:nth(2)").click(function(){
+//     $("#slide > .right > li > .guide > li:nth(2)").addClass("on").siblings().removeClass("on");
+//     $("#slide > .right > li > .textguide > li:nth(2)").addClass("on").siblings().removeClass("on");
+// });
 
-$("#slide > .control > li:nth(3)").click(function(){
-    $("#slide > .right > li > .guide > li:nth(3)").addClass("on").siblings().removeClass("on");
-    $("#slide > .right > li > .textguide > li:nth(3)").addClass("on").siblings().removeClass("on");
+// $("#slide > .control > li:nth(3)").click(function(){
+//     $("#slide > .right > li > .guide > li:nth(3)").addClass("on").siblings().removeClass("on");
+//     $("#slide > .right > li > .textguide > li:nth(3)").addClass("on").siblings().removeClass("on");
     
-});
+// });
 
-$("#slide > .control > li:nth(4)").click(function(){
-    $("#slide > .right > li > .guide > li:nth(4)").addClass("on").siblings().removeClass("on");
-    $("#slide > .right > li > .textguide > li:nth(4)").addClass("on").siblings().removeClass("on");
+// $("#slide > .control > li:nth(4)").click(function(){
+//     $("#slide > .right > li > .guide > li:nth(4)").addClass("on").siblings().removeClass("on");
+//     $("#slide > .right > li > .textguide > li:nth(4)").addClass("on").siblings().removeClass("on");
     
-});
+// });
 
-$("#slide > .control > li:nth(5)").click(function(){
-    $("#slide > .right > li > .guide > li:nth(5)").addClass("on").siblings().removeClass("on");
-    $("#slide > .right > li > .textguide > li:nth(5)").addClass("on").siblings().removeClass("on");
+// $("#slide > .control > li:nth(5)").click(function(){
+//     $("#slide > .right > li > .guide > li:nth(5)").addClass("on").siblings().removeClass("on");
+//     $("#slide > .right > li > .textguide > li:nth(5)").addClass("on").siblings().removeClass("on");
     
-});
+// });
 
-$("#slide > .control > li:nth(6)").click(function(){
-    $("#slide > .right > li > .guide > li:nth(6)").addClass("on").siblings().removeClass("on");
-    $("#slide > .right > li > .textguide > li:nth(6)").addClass("on").siblings().removeClass("on");
+// $("#slide > .control > li:nth(6)").click(function(){
+//     $("#slide > .right > li > .guide > li:nth(6)").addClass("on").siblings().removeClass("on");
+//     $("#slide > .right > li > .textguide > li:nth(6)").addClass("on").siblings().removeClass("on");
     
-});
+// });
 
-$("#slide > .control > li:nth(7)").click(function(){
-    $("#slide > .right > li > .guide > li:nth(7)").addClass("on").siblings().removeClass("on");
-    $("#slide > .right > li > .textguide > li:nth(7)").addClass("on").siblings().removeClass("on");
+// $("#slide > .control > li:nth(7)").click(function(){
+//     $("#slide > .right > li > .guide > li:nth(7)").addClass("on").siblings().removeClass("on");
+//     $("#slide > .right > li > .textguide > li:nth(7)").addClass("on").siblings().removeClass("on");
     
-});
+// });
 
-$("#slide > .control > li:nth(8)").click(function(){
-    $("#slide > .right > li > .guide > li:nth(8)").addClass("on").siblings().removeClass("on");
-    $("#slide > .right > li > .textguide > li:nth(8)").addClass("on").siblings().removeClass("on");
+// $("#slide > .control > li:nth(8)").click(function(){
+//     $("#slide > .right > li > .guide > li:nth(8)").addClass("on").siblings().removeClass("on");
+//     $("#slide > .right > li > .textguide > li:nth(8)").addClass("on").siblings().removeClass("on");
     
-});
+// });
 
 
 $("ul.guide li:first").show();
